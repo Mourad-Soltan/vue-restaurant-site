88c565a2e50e0d52e172eba4a61d88c0453e70f6
bouthaina <belgboutheina@gmail.com>
2023-12-01 06:43:13 +0100

    HomePage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="hello">
-    <p>{{ msg }}</p>
-  </div>
+  <section class="presentation">
+    <div>
+      <div class="two">
+        <h1>Enjoy
+          <span>Delicious Food</span>
+          in Your Healthy Life
+        </h1>
+      </div>
+      <p>
+        Explorez le prestige du goût au cœur de notre restaurant, où chaque plat est une œuvre d'art culinaire, 
+        savamment préparé pour éveiller vos papilles et vous offrir une expérience gastronomique inoubliable
+      </p>
+      <div>
+        <button class="btn">
+          <a class="animation" href="#"></a>
+            Visit Now
+          <a class="animation"></a>
+        </button>
+      </div>
+    </div>
+    <img src="@/assets/images/riz.png">
+  </section>
 </template>
 
 <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="home">
-    <HomePage msg="Welcome to Your Vue.js App"></HomePage>
-  </div>
+  <HomePage></HomePage>
 </template>
 
 <script>
@@ -15,3 +13,122 @@ export default {
   },
 };
 </script>
+
+<style>
+.presentation {
+  display: grid;
+  grid-template-columns: 500px auto;
+  justify-content: center;
+  gap: 50px;
+  margin-bottom: 100px;
+}
+
+.presentation img {
+  height: 500px;
+  width: 500px;
+  margin-top: -50px;
+}
+
+.presentation h1 {
+  font-size: 35px;
+  color: #F28D35;
+  margin-bottom: -70px;
+}
+
+.presentation p {
+  text-align: justify;
+  line-height: 1.2;
+  padding-top: 30px;
+}
+
+h1 {
+  top:30px;
+  position: relative;
+  padding: 0;
+  margin: 0;
+  font-family: "Raleway", sans-serif;
+  font-weight: 300;
+  font-size: 40px;
+  color: #080808;
+  -webkit-transition: all 0.4s ease 0s;
+  -o-transition: all 0.4s ease 0s;
+  transition: all 0.4s ease 0s;
+}
+
+h1 span {
+  display: block;
+  font-size: 2em;
+  line-height: 1;
+}
+h1 em {
+  font-style: normal;
+  font-weight: 900;
+}
+
+.two h1 {
+  text-transform: capitalize;
+}
+.two h1:before {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 60px;
+  height: 2px;
+  content: "";
+}
+
+.two h1 span {
+  font-size: 20px;
+  font-weight: 500;
+  text-transform: uppercase;
+  letter-spacing: 4px;
+  line-height: 3em;
+  padding-left: 0.25em;
+  color: rgba(0, 0, 0, 0.4);
+}
+.alt-two h1 {
+  text-align:center;
+}
+.alt-two h1:before {
+  left:50%; margin-left:-30px;
+}
+.btn {
+  outline: 0;
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  background: #F28D35;
+  min-width: 200px;
+  border: 0;
+  border-radius: 4px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, .1);
+  box-sizing: border-box;
+  padding: 16px 20px;
+  color: #fff;
+  font-size: 12px;
+  font-weight: 600;
+  letter-spacing: 1.2px;
+  text-transform: uppercase;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.btn:hover {
+  opacity: .95;
+}
+
+.btn .animation {
+  border-radius: 100%;
+  animation: ripple 0.6s linear infinite;
+}
+
+@keyframes ripple {
+  0% {
+    box-shadow: 0 0 0 0 rgba(255, 255, 255, 0.1), 0 0 0 20px rgba(255, 255, 255, 0.1), 0 0 0 40px rgba(255, 255, 255, 0.1), 0 0 0 60px rgba(255, 255, 255, 0.1);
+  }
+
+  100% {
+    box-shadow: 0 0 0 20px rgba(255, 255, 255, 0.1), 0 0 0 40px rgba(255, 255, 255, 0.1), 0 0 0 60px rgba(255, 255, 255, 0.1), 0 0 0 80px rgba(255, 255, 255, 0);
+  }
+}
+</style>
