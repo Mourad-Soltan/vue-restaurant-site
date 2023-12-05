22bf5d0ec5329226ff98f54d29aca72e31533e08
Mourad-Soltan <soltansoltanmourad@gmail.com>
2023-12-05 14:39:01 +0100

    fix panier

--- src/views/FoodMenu.vue
+++ src/views/FoodMenu.vue
@@ -3,6 +3,39 @@
   <section class="FoodMenu">
     <FoodMenuChild :foods="foods" @add="add" @del="del" @update-carte="update"></FoodMenuChild>
   </section>
+  <section class="panier card" v-if="paniers.length>0">
+    <div class="head">
+      <h3>FOODS</h3>
+      <h3>PRIX</h3>
+      <h3>QUANTITÉ</h3>
+      <h3>TOTAL</h3>
+    </div>
+    <hr>
+    <br>
+    <div v-for="(panier, index) in paniers" :key="index">
+      <div class="body" v-if="panier.nborder > 0">
+        <div class="profileFood">
+          <img :src="panier.image">
+          <h4>{{ panier.name }}</h4>
+        </div>
+        <h4>{{ panier.prix }}</h4>
+        <div class="quatite">
+          <span>{{ panier.nborder }}</span>
+          <div class="add_del">
+            <button v-on:click="add(panier)">+</button>
+            <button v-if="panier.nborder > 0" v-on:click="del(panier)">-</button>
+            <button v-else style="cursor: no-drop;">-</button>
+          </div>
+        </div>
+        <h4>{{ calculatePrice(panier) }}</h4>
+      </div>
+    </div>
+    <div class="Valide">
+      <span>TOTAL : {{ tolat }}</span>
+    <button class="add_to_order">Valider La Commande</button>
+    </div>
+    
+  </section>
 </template>
 
 
@@ -26,6 +59,7 @@ export default {
           image: require("../assets/images/food.jpeg"),
           p: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           nborder: 0,
+          prix: 15,
         },
         {
           id: 1,
@@ -33,6 +67,7 @@ export default {
           image: require("../assets/images/food(1).jpeg"),
           p: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           nborder: 0,
+          prix: 15,
         },
         {
           id: 2,
@@ -40,6 +75,7 @@ export default {
           image: require("../assets/images/food(2).jpeg"),
           p: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           nborder: 0,
+          prix: 15,
         },
         {
           id: 3,
@@ -47,6 +83,7 @@ export default {
           image: require("../assets/images/food(3).jpeg"),
           p: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           nborder: 0,
+          prix: 15,
         },
         {
           id: 4,
@@ -54,6 +91,7 @@ export default {
           image: require("../assets/images/food(4).jpeg"),
           p: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           nborder: 0,
+          prix: 15,
         },
         {
           id: 5,
@@ -61,6 +99,7 @@ export default {
           image: require("../assets/images/food(5).jpeg"),
           p: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           nborder: 0,
+          prix: 15,
         },
         {
           id: 6,
@@ -68,6 +107,7 @@ export default {
           image: require("../assets/images/food(6).jpeg"),
           p: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           nborder: 0,
+          prix: 15,
         },
         {
           id: 7,
@@ -75,6 +115,7 @@ export default {
           image: require("../assets/images/food(7).jpeg"),
           p: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           nborder: 0,
+          prix: 15,
         },
         {
           id: 8,
@@ -82,6 +123,7 @@ export default {
           image: require("../assets/images/food(8).jpeg"),
           p: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           nborder: 0,
+          prix: 15,
         },
         {
           id: 9,
@@ -89,6 +131,7 @@ export default {
           image: require("../assets/images/food(9).jpeg"),
           p: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           nborder: 0,
+          prix: 15,
         },
         {
           id: 10,
@@ -96,6 +139,7 @@ export default {
           image: require("../assets/images/food(10).jpeg"),
           p: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           nborder: 0,
+          prix: 15,
         },
         {
           id: 11,
@@ -103,9 +147,10 @@ export default {
           image: require("../assets/images/food(11).jpeg"),
           p: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
           nborder: 0,
+          prix: 15,
         },
       ],
-      panier:[],
+      paniers: [],
     };
   },
   methods: {
@@ -116,18 +161,37 @@ export default {
     del(food) {
       //console.log(food.nborder);
       food.nborder--;
-    },
-    update(food) {
-      for(let i=0;i<this.panier.length;i++){
-        if(this.panier[i].nborder==food.nborder){
-          this.panier.splice(i,1);
+      if (food.nborder == 0) {
+        for (let i = 0; i < this.paniers.length; i++) {
+          if (this.paniers[i].id == food.id) {
+            this.paniers.splice(i, 1);
+          }
         }
       }
-      this.panier.push(food);
-      console.log(this.panier);
     },
-
+    update(food) {
+      for (let i = 0; i < this.paniers.length; i++) {
+        if (this.paniers[i].id == food.id) {
+          this.paniers.splice(i, 1);
+        }
+      }
+      this.paniers.push(food);
+      console.log(this.paniers);
+    },
+    calculatePrice(food) {
+      return food.nborder * food.prix;
+    },
   },
+  computed:{
+    tolat(){
+      let montants=0;
+      for(let i=0;i<this.paniers.length;i++){
+        montants+=(this.paniers[i].prix*this.paniers[i].nborder);
+      }
+      return montants
+    }
+  }
+  
 };
 </script>
 
@@ -144,32 +208,7 @@ h1 {
   grid-template-columns: repeat(4, auto);
   gap: 40px;
   justify-content: center;
-  padding-bottom: 200px;
-}
-
-.card {
-  overflow: visible;
-  position: relative;
-  width: 290px;
-  height: 290px;
-  background: #fff;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, .2);
-  border-radius: 20px;
-}
-
-.card:before,
-.card:after {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 4px;
-  background: #fff;
-  transition: 0.5s;
-  z-index: -99;
-  border-radius: 20px;
+  padding-bottom: 100px;
 }
 
 .details {
@@ -237,5 +276,94 @@ h1 {
   transform: rotate(10deg);
   box-shadow: 0 2px 20px rgba(0, 0, 0, .2);
   border-radius: 20px;
+}
+
+.head {
+  display: grid;
+  grid-template-columns: 5fr 1fr 1fr 1fr;
+  gap: 50px;
+  margin-bottom: -10px;
+}
+
+.panier {
+  margin: 50px;
+  padding: 20px;
+}
+
+.panier hr {
+  border: 1px solid #ccc;
+  margin-bottom: 20px;
+
+}
+
+.body {
+  padding-bottom: 50px;
+  display: grid;
+  grid-template-columns: 5fr 1fr 1fr 1fr;
+  margin-top: -20px;
+  gap: 50px;
+}
+
+.body img {
+  width: 100px;
+  border-radius: 20px;
+}
+
+.card {
+  max-width: none;
+  height: auto;
+  background: white;
+  border-radius: 10px;
+  transition: border-radius 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.profileFood {
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+
+}
+
+.quatite {
+  display: grid;
+  grid-template-columns: 15px 20px;
+  gap: 5px;
+  margin-top: 20px;
+  padding-left: 30px;
+}
+
+.add_del {
+  display: flex;
+  flex-direction: column;
+  margin-top: -10px;
+}
+
+.add_del button {
+  border: none;
+  background-color: white;
+  cursor: pointer;
+  font-size: 15px;
+  font-weight: bold;
+}
+.Valide button{
+  border: none;
+    outline: none;
+    background-color: #F28D35;
+    padding: 10px 15px;
+    font-size: 12px;
+    font-weight: 700;
+    color: #fff;
+    border-radius: 5px;
+    transition: all ease 0.1s;
+    box-shadow: 0px 5px 0px 0px #FFE36B;
+    position: relative;
+    left:82%;
+}
+.Valide button:active {
+    transform: translateY(5px);
+    box-shadow: 0px 0px 0px 0px #a29bfe;
+}
+.Valide span{
+  position: relative;
+  left: 80%;
 }
 </style>
